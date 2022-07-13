2635f29f0713f1c38e86843db997dd17d027d955
konstantine <kbakradze@impel.io>
2022-07-13 10:13:33 +0400

    change to compare demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h3>IMPEL</h3>
+      <div className='titleAndParagraph'>
+        <h1>COMPARE VEHICLE</h1>
+        <p>DESCRIPTION</p>
+      </div>
+      <div>
+        <input type='text' placeholder='Vhicle default' className='vhicleDefault vhicleSearch'/>
+        <input type='text' placeholder='Vhicle default' className='vhicleSecond vhicleSearch'/>
+      </div>
+      <button>Compare</button>
     </div>
   );
 }
